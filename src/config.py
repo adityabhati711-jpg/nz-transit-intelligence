a8b9f6f40d3a1eb5db9330626137d6f54195eb85
Aditya Bhati <aditya.bhati01@whitireianz.ac.nz>
2026-09-18 21:21:18 +1200

    Prefer deployment environment variables over local env

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env", override=True)
+load_dotenv(BASE_DIR / ".env", override=False)
 
 
 # -----------------------------
